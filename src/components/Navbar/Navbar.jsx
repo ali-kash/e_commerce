bc38aa9b04c916f9fe653aaf94078cb1933cd392
Kai <ali.rf.kashani@gmail.com>
2021-01-19 13:24:04 -0800

    created checkout page, started to build out address form

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import {
 	AppBar,
 	Toolbar,
@@ -13,14 +14,21 @@ import useStyles from './navbarStyles'
 
 import logo from '../../assets/commerce-logo.png'
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
 	const classes = useStyles()
+	const location = useLocation()
 
 	return (
 		<>
 			<AppBar position='fixed' className={classes.appBar} color='inherit'>
 				<Toolbar>
-					<Typography variant='h6' className={classes.title} color='inherit'>
+					<Typography
+						component={Link}
+						to='/'
+						variant='h6'
+						className={classes.title}
+						color='inherit'
+					>
 						<img
 							src={logo}
 							alt='Commerce.js'
@@ -30,13 +38,20 @@ const Navbar = () => {
 						React Commerce
 					</Typography>
 					<div className={classes.grow} />
-					<div className={classes.button}>
-						<IconButton aria-label='Show cart items' color='inherit'>
-							<Badge badgeContent={2} color='secondary'>
-								<ShoppingCart />
-							</Badge>
-						</IconButton>
-					</div>
+					{location.pathname === '/' && (
+						<div className={classes.button}>
+							<IconButton
+								component={Link}
+								to='/cart'
+								aria-label='Show cart items'
+								color='inherit'
+							>
+								<Badge badgeContent={totalItems} color='secondary'>
+									<ShoppingCart />
+								</Badge>
+							</IconButton>
+						</div>
+					)}
 				</Toolbar>
 			</AppBar>
 		</>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,26 +4,7 @@ import Product from './Product/Product'
 
 import useStyles from './productsStyles'
 
-const products = [
-	{
-		id: 1,
-		name: 'Shoes',
-		description: 'Running Shoes',
-		price: '$5',
-		image:
-			'https://www.tennisnuts.com/images/product/full/Nike-Zoom-Vomero-9-Mens-Running-Shoe-642195_003_A_PREM.jpg',
-	},
-	{
-		id: 2,
-		name: 'Macbook',
-		description: 'Apple Macbook',
-		price: '$10',
-		image:
-			'https://cdn.idropnews.com/wp-content/uploads/2019/08/07094923/16-inch-macbook-pro.jpeg',
-	},
-]
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
 	const classes = useStyles()
 
 	return (
@@ -32,7 +13,7 @@ const Products = () => {
 			<Grid container justify='center' spacing={4}>
 				{products.map((product) => (
 					<Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-						<Product product={product} />
+						<Product product={product} onAddToCart={onAddToCart} />
 					</Grid>
 				))}
 			</Grid>
